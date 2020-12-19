d2141c7acf1694f9d6072eebe1a25c5de5935804
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-12-19 18:39:25 +0700

    fix url server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://stories-mern-stack-app.herokuapp.com";
+const url = "https://stories-mern-stack-app.herokuapp.com/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
