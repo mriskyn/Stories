9b2e332229d79af3bd2c42742bd98b80fef4c7b7
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-12-19 18:36:27 +0700

    change server url

--- server/app.js
+++ server/app.js
@@ -11,18 +11,13 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL =
-  // "mongodb+srv://rsk:rskngrh@cluster1.d2szq.gcp.mongodb.net/Stories?retryWrites=true&w=majority";
-
-  mongoose
-    .connect(process.env.CONNECTION_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() =>
-      app.listen(PORT, () => console.log("Listening on port :", PORT))
-    )
-    .catch((err) => console.log(err));
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(PORT, () => console.log("Listening on port :", PORT)))
+  .catch((err) => console.log(err));
 
 app.use(router);
 
