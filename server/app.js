9953292599144768e974377dbadc5f7e63fcbd82
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-12-19 17:27:11 +0700

    added environment in server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const router = require("./routes");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const app = express();
 
@@ -11,12 +12,17 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL =
-  "mongodb+srv://rsk:rskngrh@cluster1.d2szq.gcp.mongodb.net/Stories?retryWrites=true&w=majority";
+  // "mongodb+srv://rsk:rskngrh@cluster1.d2szq.gcp.mongodb.net/Stories?retryWrites=true&w=majority";
 
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log("Listening on port :", PORT)))
-  .catch((err) => console.log(err));
+  mongoose
+    .connect(process.env.CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() =>
+      app.listen(PORT, () => console.log("Listening on port :", PORT))
+    )
+    .catch((err) => console.log(err));
 
 app.use(router);
 
